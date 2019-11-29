dc5f1fe10463cef146733369fa409da6b0318ae1
jimin <jimin@1token.trade>
2019-11-29 16:23:15 +0800

    update: __version__

--- onetoken_sync/__init__.py
+++ onetoken_sync/__init__.py
@@ -7,4 +7,4 @@ from .rpcutil import Error, HTTPError, Code, Const
 from .quote import Quote, get_client, subscribe_tick, get_v3_client, subscribe_tick_v3, get_candle_client, \
     subscribe_candle, get_zhubi_client, subscribe_zhubi, get_last_tick, get_contracts, get_contract
 
-__version__ = '0.2.191107.0'
+__version__ = '0.2.191129.0'
